4db11ae1112c31cbe40a2eab86feaf2df0cb51dc
Hennadii Borysevych <genuch3@gmail.com>
2024-11-06 15:20:14 +0100

    routing adjusted

--- routing/producer.py
+++ routing/producer.py
@@ -1,9 +1,6 @@
 import pika
 from pika.exchange_type import ExchangeType
 
-def on_message_received(ch, method, properties, body):
-    print(f'Payments - received new message: {body}')
-
 connection_parameters = pika.ConnectionParameters('localhost')
 
 connection = pika.BlockingConnection(connection_parameters)
@@ -12,13 +9,10 @@ channel = connection.channel()
 
 channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
 
-queue = channel.queue_declare(queue='', exclusive=True)
+message = 'This message needs to be routed'
 
-channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='paymentsonly')
-channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='both')
-channel.basic_consume(queue=queue.method.queue, auto_ack=True,
-    on_message_callback=on_message_received)
+channel.basic_publish(exchange='routing', routing_key='both', body=message)
 
-print('Payments Starting Consuming')
+print(f'sent message: {message}')
 
-channel.start_consuming()
+connection.close() 
